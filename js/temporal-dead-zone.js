079363907775018d070e3f03b900ad0b952059bf
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2018-01-28 19:18:05 +0000

    Add Temporal Dead Zone

---
+++ js/temporal-dead-zone.js
@@ -0,0 +1,9 @@
+// You can access the variable,
+// however you can't access the value of it
+// console.log(pizza);
+// var pizza = 'Deep Dish';
+
+// You can access the variable or its value
+// this zone is cosidered a 'dead zone'
+console.log(pizza);
+let pizza = 'Deep Dish';
